690423ee087754ed7fe8d6945d6073d9bed04ba5
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2023-10-22 15:37:41 +0300

    Added TicTacToe component logic and interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import TicTacToe from "./components/TicTacToe"
+
 function App() {
 
   return (
-    <>
-    </>
+    <div className="border rounded-xl w-96 h-full"  >
+      <TicTacToe />
+    </div>
   )
 }
 
